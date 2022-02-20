dbab67f7ab94b33570fbfb4a1ea310896043fb28
ManideepBangaru <bmd994@gmail.com>
2022-02-21 00:01:25 +0530

    added a home page

--- NewSite/NewSite/urls.py
+++ NewSite/NewSite/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# Importing Apps we created
+from calc import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.home,name="home")
 ]
