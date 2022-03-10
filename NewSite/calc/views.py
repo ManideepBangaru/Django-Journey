122f6271884cef70e8ceee5cf222f28fdf4e7b9d
ManideepBangaru <bmd994@gmail.com>
2022-03-10 11:25:28 +0530

    updated

--- NewSite/calc/views.py
+++ NewSite/calc/views.py
@@ -11,14 +11,14 @@ def home(request):
 
 def add(request):
     operationType = "Addition"
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     addvalue = val1 + val2
     return render(request, "result.html", {'result': addvalue, "val1" : val1, "val2":val2,"op":operationType})
 
 def Subtract(request):
     operationType = "Subtraction"
-    val1 = int(request.GET['num1'])
-    val2 = int(request.GET['num2'])
+    val1 = int(request.POST['num1'])
+    val2 = int(request.POST['num2'])
     subvalue = val1 - val2
     return render(request, "result.html", {'result': subvalue, "val1" : val1, "val2":val2,"op":operationType})
