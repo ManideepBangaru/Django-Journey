aa91a07a90ad7f664dcf291b937532935af26c52
ManideepBangaru <bmd994@gmail.com>
2022-10-05 03:02:49 +0530

    updated calc app

--- projects/projectLearn/projectLearn/urls.py
+++ projects/projectLearn/projectLearn/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from calc import views as calcViews
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("hello/", calcViews.hello_world),
 ]
